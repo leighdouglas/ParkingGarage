d660b07fb5c0f67be962d16230883473d9b07442
Abass <abass.bayo@gmail.com>
2016-09-11 15:10:23 -0400

    added Driveable class

---
+++ src/nyc/c4q/abassawo/Main.java
@@ -0,0 +1,15 @@
+package nyc.c4q.abassawo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ParkingGarage garage = new ParkingGarage(4);
+        System.out.println(garage);
+    }
+}
